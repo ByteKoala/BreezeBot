4f642f96998ae758e3843fa2097d59ac3c0933e9
ken <Kendrick4Jobs@protonmail.com>
2024-10-10 17:33:12 +0800

    Fix env  bugs

--- app/GoogleAction.ts
+++ app/GoogleAction.ts
@@ -3,8 +3,8 @@ import {
 } from "@google/generative-ai";
 
 const MODEL_NAME = "gemini-1.5-pro";
-//const GOOGLE_GENERATIVE_AI_API_KEY = "";
-const GOOGLE_GENERATIVE_AI_API_KEY = process.env.GOOGLE_GENERATIVE_AI_API_KEY;
+const GOOGLE_GENERATIVE_AI_API_KEY = "";
+//const GOOGLE_GENERATIVE_AI_API_KEY = process.env.GOOGLE_GENERATIVE_AI_API_KEY;
 
 async function executeConversation(request: string[]) {
   const genAI = new GoogleGenerativeAI(GOOGLE_GENERATIVE_AI_API_KEY as string);
